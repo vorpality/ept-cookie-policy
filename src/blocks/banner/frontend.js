8c13a73c754ea2324ff795a12515493d7f1db5d9
vorpality <kaz.s12b@gmail.com>
2024-01-26 01:51:10 +0200

    Update frontend.js

--- src/blocks/banner/frontend.js
+++ src/blocks/banner/frontend.js
@@ -2,22 +2,23 @@
 document.addEventListener('DOMContentLoaded', () => {
   const banner = document.querySelector('.wp-block-ept-cookie-banner .content');
   const acceptButton = document.querySelector('#accept-cookies');
-  console.log(acceptButton)
-  acceptButton.addEventListener('click',event =>{
-    event.preventDefault();
-    console.log('clock');
-    var expires = new Date();
-    expires.setMonth(expires.getMonth() + 12);
-    document.cookie = 'policy=accept;expires='+expires+';path=/'
-    banner.classList.add('hidden');
-  })
-  const rejectButton = document.querySelector('#reject-cookies');
-  rejectButton.addEventListener('click',event =>{
-    event.preventDefault();
-    
-    var expires = new Date();
-    expires.setMonth(expires.getMonth() + 12);
-    document.cookie = 'policy=reject;expires='+expires+';path=/'
-    banner.classList.add('hidden');
-  })
+  if(acceptButton != null){
+    acceptButton.addEventListener('click',event =>{
+      event.preventDefault();
+      console.log('clock');
+      var expires = new Date();
+      expires.setMonth(expires.getMonth() + 12);
+      document.cookie = 'policy=accept;expires='+expires+';path=/'
+      banner.classList.add('hidden');
+    })
+    const rejectButton = document.querySelector('#reject-cookies');
+    rejectButton.addEventListener('click',event =>{
+      event.preventDefault();
+      
+      var expires = new Date();
+      expires.setMonth(expires.getMonth() + 12);
+      document.cookie = 'policy=reject;expires='+expires+';path=/'
+      banner.classList.add('hidden');
+    })
+  }
 })
